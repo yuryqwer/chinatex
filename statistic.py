63fcc06d4755282eccd3f8db0793075f7be61ff6
yuryqwer <yuruqwer@outlook.com>
2024-02-04 21:01:31 +0800

    修改汉字统计代码

--- statistic.py
+++ statistic.py
@@ -1,34 +1,28 @@
 import os
 import unicodedata
+from pprint import pprint
 
 
 def chinese():
     chinese_count = 0
-    chinese_word = set()
+    chinese_word = {}
 
     for filename in os.listdir("./real_captcha"):
         color, text, *_ = filename[:-4].split("_")
         for c in text:
             if 'CJK' in unicodedata.name(c):
                 chinese_count += 1
-                chinese_word.add(c)
+                chinese_word.setdefault(c, 0)
+                chinese_word[c] += 1
 
     return len(chinese_word), chinese_count, chinese_word
 
 
-def simulate(word_count, chinese_count):
-    import random
-    s = [i for i in range(word_count)]
-    chinese_word = set()
-    for _ in range(chinese_count):
-        chinese_word.add(random.choice(s))
-    return len(chinese_word)
-
-
 length, count, word = chinese()
-print(length, simulate(700, count))
+print("目前一共出现的汉字数量：", length)
 
 with open("./font/chinese7000.txt", encoding="utf-8") as f:
     word_list = [i for i in f.readline()]
 
-print(sorted(word_list.index(w) for w in word))
+print("索引", "出现次数")
+pprint(sorted((word_list.index(w), word[w]) for w in word))
